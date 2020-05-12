2b255223a1fea8cc32dc019aa014fdae838d38cc
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-12 19:11:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_e_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal n1;
+
+            Console.Write("Digite um numero: ");
+            n1 = Convert.ToDecimal(Console.ReadLine());
+
+            if (n1 % 2 == 0)
+                Console.Write("Seu numero e par");
+            else
+                Console.Write("Seu numero e impar");
+
+            Console.Read();
+
+        }
+    }
+}
